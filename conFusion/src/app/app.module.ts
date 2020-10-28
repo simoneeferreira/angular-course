02b013feadaf3350ca200bde74b445cfae6ab0b5
simoneeferreira <simoneef1986@gmail.com>
2020-10-28 18:08:55 +0000

    fixed

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -10,18 +10,24 @@ import { MatButtonModule } from '@angular/material/button';
 
 
 
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { DishService } from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
